e9cd6a58b87336f81964f8e043df714ea5fec4bf
yuga-92 <yurahalych@gmail.com>
2016-01-08 01:42:28 +0200

    Comments removed

--- Array_bubble_sorting/main.c
+++ Array_bubble_sorting/main.c
@@ -9,7 +9,24 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int arr1 [] = {1,5,3,7,4,23,55,9, 0, 55};
+    for (int i =0; i < (sizeof(arr1)/sizeof(arr1[0])); i++){
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+    for (int i = sizeof(arr1)/sizeof(arr1[0])-1; i>=0; i--){
+        for (int j=0; j<i; j++){
+            if (arr1[j]> arr1[j+1]){
+                int temp = arr1[j+1];
+                arr1[j+1] = arr1[j];
+                arr1[j]= temp;
+            }
+        }
+    }
+    for (int i =0; i< (sizeof(arr1)/sizeof(arr1[0])); i++){
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
     return 0;
 }
